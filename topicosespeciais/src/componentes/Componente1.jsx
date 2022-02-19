eec089905719e8e501c2cb6b433dd2d3cbe7c5b9
Centeno-76 <mateuscenteno76@gmail.com>
2022-02-19 15:48:52 +0000

    primeira atualização

---
+++ topicosespeciais/src/componentes/Componente1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function func1(){
+//     return <h1>PRIMEIRO COMPONENTE</h1>
+// }
+// export default func1
+
+export default () => <h1>Nosso componente Arrow</h1>
+// export default () => (3+7)
---
+++ topicosespeciais/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import  Component1  from "./componentes/Componente1";
+
+const element = document.getElementById('root')
+
+ReactDOM.render(
+        <div>
+            <Component1 />
+        </div>
+    , element
+)
